867cb58c180e9960547b0bb6b42af8f011708824
Crowdbotics <team@crowdbotics.com>
2023-10-05 17:15:29 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "minds-alike-43774",
+  url: "https://minds-alike-43774.botics.co",
+  api: "https://minds-alike-43774.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
